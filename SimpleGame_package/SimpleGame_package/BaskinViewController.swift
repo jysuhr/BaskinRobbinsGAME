79b32b34b407ed1d1711a498e07d5d8555e30fdf
jysuhr <jun0.suhr@gmail.com>
2024-08-24 19:29:31 +0900

    버튼 레이아웃 + 숫자오르는 기능 구현

--- SimpleGame_package/SimpleGame_package/BaskinViewController.swift
+++ SimpleGame_package/SimpleGame_package/BaskinViewController.swift
@@ -19,17 +19,20 @@ class BaskinViewController: UIViewController {
     let play2Button2 = UIButton()
     let play2Button3 = UIButton()
     
+    var gameNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = bgColor2
         
         circlePanelSetup()
         gameNumLabelSetup()
+        playButtonSetup()
         
     }
     
     func circlePanelSetup() {
-        circlePanel.backgroundColor = laColor1
+        circlePanel.backgroundColor = laColor1 // 1, 2, 3, 4, 5
         circlePanel.layer.cornerRadius = 250 / 2
         circlePanel.clipsToBounds = true
         self.view.addSubview(circlePanel)
@@ -42,7 +45,7 @@ class BaskinViewController: UIViewController {
     }
     
     func gameNumLabelSetup() {
-        gameNumLabel.text = "0"
+        gameNumLabel.text = "\(gameNumber)"
         gameNumLabel.textColor = numColor1
         gameNumLabel.font = UIFont.systemFont(ofSize: 100)
         gameNumLabel.textAlignment = .center
@@ -54,12 +57,74 @@ class BaskinViewController: UIViewController {
         }
     }
     
-    func play11Setup() {
+    func playButtonSetup() {
+        let buttons = [play1Button1, play1Button2, play1Button3, play2Button1, play2Button2, play2Button3]
+        buttons.forEach {
+            $0.backgroundColor = playColor
+            $0.layer.cornerRadius = 16
+            $0.setTitleColor(self.laColor1, for: .normal)
+            $0.titleLabel?.font = UIFont.systemFont(ofSize: 50)
+            $0.addTarget(self, action: #selector(buttonTapped(_: )), for: .touchUpInside)
+            $0.addTarget(self, action: #selector(buttonHighlighted(_:)), for: [.touchDown, .touchDragEnter])
+            $0.addTarget(self, action: #selector(buttonNormal(_:)), for: [.touchUpInside, .touchDragExit, .touchCancel])
+            self.view.addSubview($0)
+        }
         
+        play1Button1.setTitle("1", for: .normal)
+        play1Button2.setTitle("2", for: .normal)
+        play1Button3.setTitle("3", for: .normal)
+        play2Button1.setTitle("1", for: .normal)
+        play2Button2.setTitle("2", for: .normal)
+        play2Button3.setTitle("3", for: .normal)
+
+        
+        play1Button1.snp.makeConstraints {
+            $0.top.equalTo(circlePanel.snp.bottom).offset(120)
+            $0.leading.equalTo(self.view.snp.leading).offset(50)
+            $0.width.height.equalTo(90)
+        }
+        play1Button2.snp.makeConstraints {
+            $0.top.equalTo(play1Button1.snp.bottom).offset(20)
+            $0.leading.equalTo(self.view.snp.leading).offset(50)
+            $0.width.height.equalTo(90)
+        }
+        play1Button3.snp.makeConstraints {
+            $0.top.equalTo(play1Button2.snp.bottom).offset(20)
+            $0.leading.equalTo(self.view.snp.leading).offset(50)
+            $0.width.height.equalTo(90)
+        }
+        play2Button1.snp.makeConstraints {
+            $0.top.equalTo(circlePanel.snp.bottom).offset(120)
+            $0.trailing.equalTo(self.view.snp.trailing).offset(-50)
+            $0.width.height.equalTo(90)
+        }
+        play2Button2.snp.makeConstraints {
+            $0.top.equalTo(play2Button1.snp.bottom).offset(20)
+            $0.trailing.equalTo(self.view.snp.trailing).offset(-50)
+            $0.width.height.equalTo(90)
+        }
+        play2Button3.snp.makeConstraints {
+            $0.top.equalTo(play2Button2.snp.bottom).offset(20)
+            $0.trailing.equalTo(self.view.snp.trailing).offset(-50)
+            $0.width.height.equalTo(90)
+        }
     }
-
-
-
+    // 버튼 누름 효과
+    @objc func buttonHighlighted(_ sender: UIButton) {
+        sender.backgroundColor = playColor.withAlphaComponent(0.7) // 버튼이 눌렸을 때의 색상
+    }
+    @objc func buttonNormal(_ sender: UIButton) {
+        sender.backgroundColor = playColor // 버튼이 정상 상태일 때의 색상
+    }
+    // Button Action
+    @objc func buttonTapped(_ sender: UIButton) {
+        // 버튼의 타이틀을 숫자로 변환하여 gameNumber를 증가시킵니다
+        if let buttonTitle = sender.title(for: .normal),
+           let buttonNumber = Int(buttonTitle) {
+            gameNumber += buttonNumber
+            gameNumLabel.text = "\(gameNumber)"
+        }
+    }
 }
 
 
@@ -87,7 +152,7 @@ struct BaskinViewControllerRepresentable_PreviewProvider: PreviewProvider {
         Group {
             BaskinViewControllerRepresentable()
                 .ignoresSafeArea()
-                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDisplayName("BaskinVCPreView")
                 .previewDevice(PreviewDevice(rawValue: "iPhone 15 Pro"))
         }
         
